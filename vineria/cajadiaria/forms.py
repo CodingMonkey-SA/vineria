eadac6182b6ad6eb0aa02c151a0868eba73739bb
gabohernandez <hernandezmaurog@gmail.com>
2016-02-01 20:03:02 -0300

    remove vineria

--- vineria/cajadiaria/forms.py
+++
@@ -1,9 +0,0 @@
-from cajadiaria.models import CajaDiaria
-from django import forms
-from django.forms import ModelForm
-
-class CajaDiariaForm(forms.ModelForm):
-
-    class Meta:
-        model = CajaDiaria
-        fields = ('saldoinicial',)
